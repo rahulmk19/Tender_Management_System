f2ed6e6a1fb24980361b42b79b58e004e231241a
unknown <rahulmk.94802@gmail.com>
2023-03-30 00:35:13 +0530

    Project Started

--- Tender_Management_System/src/com/masai/ui/Main.java
+++ Tender_Management_System/src/com/masai/ui/Main.java
@@ -1,11 +1,46 @@
-package com.masai.ui;
+package com.masai.UI;
+
+import java.util.Scanner;
 
 public class Main {
+	
+	static void Vendor(Scanner sc) {
+		System.out.println("Welcome Vendor");
+		
+	}
+
+	static void Administrator(Scanner sc) {
+		System.out.println("*-*-*-*-*-*-*-*-*-*-*-*-*-***-");
+		System.out.println("Welcome Administrator");
+		
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello");
-
+		Scanner sc=new Scanner(System.in);
+		int choice=0;
+		
+		do {
+			System.out.println("1. Administrator");
+			System.out.println("2. Vendor");
+			
+			choice=sc.nextInt();
+			switch(choice) {
+				case 1:
+					Administrator(sc);
+					break;
+					
+				case 2:
+					Vendor(sc);
+					break;
+					
+				default:
+					System.out.println("Please Select Correct Option");
+			}
+		}
+		while(choice!=0);
+		sc.close();
 	}
+
+
 
 }
